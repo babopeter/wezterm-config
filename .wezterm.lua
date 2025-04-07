c4f5116a7425b4239260a5502028c97172a1805d
Peter Babo <33118245+babopeter@users.noreply.github.com>
2025-04-07 10:07:26 +0200

    Add .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,161 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+local gpus = wezterm.gui.enumerate_gpus()
+
+config.front_end = "OpenGL"
+config.max_fps = 144
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 1
+config.cursor_blink_rate = 500
+config.term = "xterm-256color" -- Set the terminal type
+
+config.font = wezterm.font("JetBrains Mono Regular")
+config.cell_width = 0.9
+config.window_background_opacity = 0.9
+config.prefer_egl = true
+config.font_size = 12.0
+
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
+}
+
+-- hide tabs
+-- config.hide_tab_bar_if_only_one_tab = true
+-- config.use_fancy_tab_bar = false
+-- config.tab_bar_at_bottom = true
+
+-- config.inactive_pane_hsb = {
+-- 	saturation = 0.0,
+-- 	brightness = 1.0,
+-- }
+
+-- color scheme toggling
+wezterm.on("toggle-colorscheme", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	if overrides.color_scheme == "Zenburn" then
+		overrides.color_scheme = "Cloud (terminal.sexy)"
+	else
+		overrides.color_scheme = "Zenburn"
+	end
+	window:set_config_overrides(overrides)
+end)
+
+-- keymaps
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.EmitEvent("toggle-colorscheme"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "P",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowLauncher},
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
+}
+
+-- Color scheme:
+config.color_scheme = "Cloud (terminal.sexy)"
+config.colors = {
+	-- background = '#3b224c',
+	-- background = "#181616", -- vague.nvim bg
+	-- background = "#080808", -- almost black
+	background = "#0c0b0f", -- dark purple
+	-- background = "#020202", -- dark purple
+	-- background = "#17151c", -- brighter purple
+	-- background = "#16141a",
+	-- background = "#0e0e12", -- bright washed lavendar
+	-- background = 'rgba(59, 34, 76, 100%)',
+	cursor_border = "#bea3c7",
+	-- cursor_fg = "#281733",
+	cursor_bg = "#bea3c7",
+	-- selection_fg = '#281733',
+
+	tab_bar = {
+		background = "#0c0b0f",
+		-- background = "rgba(0, 0, 0, 0%)",
+		active_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#bea3c7",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#f8f2f5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+
+		new_tab = {
+			-- bg_color = "rgba(59, 34, 76, 50%)",
+			bg_color = "#0c0b0f",
+			fg_color = "white",
+		},
+	},
+}
+
+config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+-- config.window_decorations = "NONE | RESIZE"
+config.default_prog = { "powershell.exe", "-NoLogo" }
+config.initial_cols = 80
+-- config.window_background_image = "C:/users/babop/Backgrounds/ED6GtF6.jpeg"
+-- config.window_background_image_hsb = {
+	-- brightness = 0.1,
+-- }
+
+return config
