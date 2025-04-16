52438971ab7450bbc78521344871187e355551bc
Peter Babo <33118245+babopeter@users.noreply.github.com>
2025-04-16 12:44:33 +0200

    Remove unused/comments

--- .wezterm.lua
+++ .wezterm.lua
@@ -23,16 +23,6 @@ config.window_padding = {
 	bottom = 10,
 }
 
--- hide tabs
--- config.hide_tab_bar_if_only_one_tab = true
--- config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
-
--- config.inactive_pane_hsb = {
--- 	saturation = 0.0,
--- 	brightness = 1.0,
--- }
-
 -- color scheme toggling
 wezterm.on("toggle-colorscheme", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
@@ -107,23 +97,12 @@ config.keys = {
 -- Color scheme:
 config.color_scheme = "Cloud (terminal.sexy)"
 config.colors = {
-	-- background = '#3b224c',
-	-- background = "#181616", -- vague.nvim bg
-	-- background = "#080808", -- almost black
 	background = "#0c0b0f", -- dark purple
-	-- background = "#020202", -- dark purple
-	-- background = "#17151c", -- brighter purple
-	-- background = "#16141a",
-	-- background = "#0e0e12", -- bright washed lavendar
-	-- background = 'rgba(59, 34, 76, 100%)',
 	cursor_border = "#bea3c7",
-	-- cursor_fg = "#281733",
 	cursor_bg = "#bea3c7",
-	-- selection_fg = '#281733',
 
 	tab_bar = {
 		background = "#0c0b0f",
-		-- background = "rgba(0, 0, 0, 0%)",
 		active_tab = {
 			bg_color = "#0c0b0f",
 			fg_color = "#bea3c7",
@@ -142,7 +121,6 @@ config.colors = {
 		},
 
 		new_tab = {
-			-- bg_color = "rgba(59, 34, 76, 50%)",
 			bg_color = "#0c0b0f",
 			fg_color = "white",
 		},
@@ -150,13 +128,8 @@ config.colors = {
 }
 
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
--- config.window_decorations = "NONE | RESIZE"
 config.default_prog = { "powershell.exe", "-NoLogo" }
 config.initial_cols = 80
--- config.window_background_image = "C:/users/babop/Backgrounds/ED6GtF6.jpeg"
--- config.window_background_image_hsb = {
-	-- brightness = 0.1,
--- }
 
 config.launch_menu = {
 	{
